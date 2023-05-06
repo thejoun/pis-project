afd5a1779045e7f98d0d58cb9f3a40a0519bfc73
jgolynsk <01158853@pw.edu.pl>
2023-05-06 19:22:21 +0200

    spring new project added

---
+++ pisjava/src/main/java/pis/pisjava/PisjavaApplication.java
@@ -0,0 +1,13 @@
+package pis.pisjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisjavaApplication.class, args);
+	}
+
+}
