34a333b1f40a3f445a7322d7806e87063266acb6
thejoun <the.jonu@gmail.com>
2023-05-25 23:52:51 +0200

    [client] temporarily hardcoded service urls

---
+++ SocialApp/BlazorClient/Config/Services.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorClient.Config;
+
+/// <summary>
+/// Afaik, Blazor WebAssembly can't handle environment variables.
+/// This is only a temporary solution.
+/// </summary>
+public static class Services
+{
+    public static string UserTimeline => "http://pisproject.eastus.azurecontainer.io:5001";
+    public static string UserProfile => "http://pisproject.eastus.azurecontainer.io:5002";
+}
--- SocialApp/BlazorClient/Program.cs
+++ SocialApp/BlazorClient/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorClient;
+using BlazorClient.Config;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var rootComponents = builder.RootComponents;
@@ -9,7 +10,7 @@ var services = builder.Services;
 rootComponents.Add<App>("#app");
 rootComponents.Add<HeadOutlet>("head::after");
 
-services.AddScoped(provider => new HttpClient
+services.AddScoped(_ => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
@@ -19,21 +20,14 @@ services.AddOidcAuthentication(options =>
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
 
-// these seemed not to work when set from docker-compose
-// var userTimeline = Environment.GetEnvironmentVariable("USER_TIMELINE_SERVICE");
-// var userProfile = Environment.GetEnvironmentVariable("USER_PROFILE_SERVICE");
-
-var userTimeline = "http://pisproject.eastus.azurecontainer.io:5001";
-var userProfile = "http://pisproject.eastus.azurecontainer.io:5002";
-
 // custom
 services.AddHttpClient("UserTimelineService", client =>
 {
-    client.BaseAddress = new Uri(userTimeline);
+    client.BaseAddress = new Uri(Services.UserTimeline);
 });
 services.AddHttpClient("UserProfileService", client =>
 {
-    client.BaseAddress = new Uri(userProfile);
+    client.BaseAddress = new Uri(Services.UserProfile);
 });
 
 await builder.Build().RunAsync();
