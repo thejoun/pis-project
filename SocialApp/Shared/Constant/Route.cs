32ff46ab91445b2dcdc5167318f50eb7d4a040d6
thejoun <the.jonu@gmail.com>
2023-06-03 16:47:27 +0200

    [client] improved user profile setup flow

--- SocialApp/Shared/Constant/Route.cs
+++ SocialApp/Shared/Constant/Route.cs
@@ -11,14 +11,16 @@ public static class Route
     public const string HasProfileWithSub = "HasProfileWithSub";
     public const string HasProfileWithHandle = "HasProfileWithHandle";
     public const string GetPostsForUserHandle = "GetPosts";
+    public const string AddPost = "AddPost";
     
     public static class Request
     {
         public const string GetProfileWithHandle = Profile + "/" + Route.GetProfileWithHandle + "?user=";
-        public const string GetProfileWithSub = Profile + "/" + Route.GetProfileWithSub + "?email=";
+        public const string GetProfileWithSub = Profile + "/" + Route.GetProfileWithSub + "?sub=";
         public const string CreateProfile = Profile + "/" + Route.CreateProfile;
         public const string HasProfileWithSub = Profile + "/" + Route.HasProfileWithSub + "?sub=";
         public const string HasProfileWithHandle = Profile + "/" + Route.HasProfileWithHandle + "?handle=";
         public const string GetPostsForUserHandle = Timeline + "/" + Route.GetPostsForUserHandle + "?user=";
+        public const string AddPost = Timeline + "/" + Route.AddPost;
     }
 }
