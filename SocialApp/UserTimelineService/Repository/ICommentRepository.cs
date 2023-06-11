74fe07208c1c4ccf3a7c17341ea21a65c62ca657
mmarfenk <mmarfenk@>
2023-06-11 22:48:58 +0200

    Added option to remove like from comments

--- SocialApp/UserTimelineService/Repository/ICommentRepository.cs
+++ SocialApp/UserTimelineService/Repository/ICommentRepository.cs
@@ -5,10 +5,9 @@ namespace UserTimelineService.Repository
     public interface ICommentRepository
     {
         public Task<IReadOnlyCollection<Comment>> GetComments();
-        public Task<IReadOnlyCollection<Comment>> GetComments(int postId);
         public Task AddComment(Comment comment);
-        public Task DeleteComment(int commentId);
         public Task AddLike(Comment comment);
+        public Task RemoveLike(Comment comment);
         public Task<IReadOnlyCollection<Comment>> GetHomeTimelineForUserComment(int skip, int take);
     }
 }
